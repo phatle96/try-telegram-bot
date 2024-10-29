8df7372bd06990ceef14d6889f5ec68bce3e9965
phatle96 <phatlv96@gmail.com>
2024-10-29 17:04:34 +0700

    First Version

---
+++ bot.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+const app = express();
+app.use(bodyParser.json());
+
+let users = [];
+const fs = require('fs');
+const usersFile = 'users.json';
+
+// Load or initialize user data
+if (fs.existsSync(usersFile)) {
+    users = JSON.parse(fs.readFileSync(usersFile));
+}
+
+function saveUsers() {
+    fs.writeFileSync(usersFile, JSON.stringify(users, null, 2));
+}
+
+function addUser(chatId, username, emailAddress) {
+    const userExists = users.some((user) => user.chatId === chatId);
+    if (!userExists) {
+        users.push({ chatId, username, emailAddress });
+        saveUsers();
+    }
+}
+
+// Import listeners and API routes
+require('./listeners/start')(bot, addUser);
+require('./listeners/message')(bot); // for other messages
+require('./apis/email')(app, bot, users, saveUsers);
+
+// Start the Express server
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
